6c0639f9a8dcf80edf429d8c6feb1062fc98952c
Bert Kenward <bert.kenward@gmail.com>
2015-04-09 22:26:02 +0100

    Admin: Add jquery

--- admin/index.php
+++ admin/index.php
@@ -20,6 +20,8 @@ function h($t)
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Administration - Cambridge Beer Festival Volunteering</title>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 </head>
 <body>
 <header>
