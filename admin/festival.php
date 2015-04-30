c89092a042a5c679281a96c3557c8800a5324f46
Bert Kenward <bert.kenward@gmail.com>
2015-04-30 22:01:13 +0100

    Add festival JSON supplier

---
+++ admin/festival.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('admin-header.inc.php');
+require_once('festival.inc.php');
+
+if (!isset($_GET['festival']) || !preg_match('/^\S+$/', $_GET['festival'])) {
+	$f = Festival::current();
+} else {
+	$f = Festival::from_tag($_GET['festival']);
+}
+
+if (!$f) {
+	http_response_code(404);
+	exit(0);
+}
+
+echo json_encode($f);
