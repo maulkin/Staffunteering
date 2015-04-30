fae9a8444c2b93b886534c91c385c45474e6d19a
Bert Kenward <bert.kenward@gmail.com>
2015-04-30 22:03:54 +0100

    Admin: WIP - add individual volunteer getter

---
+++ admin/volunteer-single.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('admin-header.inc.php');
+require_once('festival.inc.php');
+require_once('person.inc.php');
+require_once('person-festival.inc.php');
+
+if (!isset($_GET['person']) || !preg_match('/^\d+$/', $_GET['person'])) {
+	http_response_code(400);
+	exit(0);
+}
+
+$p = new Person($_GET['person']);
+
+if (!isset($_GET['festival']) || !preg_match('/^\S+$/', $_GET['festival'])) {
+	$f = Festival::current();
+} else {
+	$f = Festival::from_tag($_GET['festival']);
+}
+
+if (!$p || !$f) {
+	http_response_code(404);
+	exit(0);
+}
+
+$pf = new PersonFestival($p, $f);
+
+/* Return specific elements. TODO: add more. */
+$p_filter = [
+	"name" => $p->name,
+	"email" => $p->email
+	];
+
+echo json_encode([
+	"person" => $p_filter,
+	"sessions" => $pf->sessions,
+	"flags" => $pf->flags]);
